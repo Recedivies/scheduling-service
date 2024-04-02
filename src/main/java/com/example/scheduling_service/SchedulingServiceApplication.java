52e9b33297fd9237008784d455153d27c9f592ed
recedivies <ahmadhiprananta@gmail.com>
2024-04-02 13:42:23 +0700

    Update Dockerfile

--- src/main/java/com/example/scheduling_service/SchedulingServiceApplication.java
+++ src/main/java/com/example/scheduling_service/SchedulingServiceApplication.java
@@ -1,13 +1,20 @@
 package com.example.scheduling_service;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SchedulingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchedulingServiceApplication.class, args);
+		ApplicationContext applicationContext = new SpringApplicationBuilder(SchedulingServiceApplication.class).run(args);
+		Environment environment = applicationContext.getEnvironment();
+		System.out.println("[DEBUG]" + environment.getProperty("spring.datasource.url"));
+		System.out.println("[DEBUG]" + environment.getProperty("spring.datasource.username"));
+		System.out.println("[DEBUG]" + environment.getProperty("spring.datasource.password"));
+		System.out.println("[DEBUG]" + environment.getProperty("spring.rabbitmq.host"));
 	}
 
 }
