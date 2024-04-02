deb4da220928636e1af53ae31deafbb67d39e8ef
recedivies <ahmadhiprananta@gmail.com>
2024-04-03 06:54:18 +0700

    Update CI

--- src/main/java/com/example/scheduling_service/config/rabbit/RabbitMQConfig.java
+++ src/main/java/com/example/scheduling_service/config/rabbit/RabbitMQConfig.java
@@ -3,37 +3,51 @@ package com.example.scheduling_service.config.rabbit;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    public static final String TASK_DIRECT_EXCHANGE = "x.task";
-    public static final String QUEUE_TASK = "q.task";
-    public static final String QUEUE_TASK_RETRY = "q.task-retry";
+    public static final String TASK_DIRECT_EXCHANGE = "x.manja";
+    public static final String QUEUE_TASK = "q.maja";
+//    public static final String QUEUE_TASK_RETRY = "q.task-retry";
 
-    public static final String ROUTING_KEY_TASK = "task";
-    public static final String ROUTING_KEY_TASK_RETRY = "task-retry";
+    public static final String ROUTING_KEY_TASK = "manja";
+//    public static final String ROUTING_KEY_TASK_RETRY = "task-retry";
 
     @Bean
-    public Declarables createTaskScheduleSchema() {
-        System.out.println("[DEBUG] HELLO DECLARE X & Q");
-        return new Declarables(
-                new DirectExchange(TASK_DIRECT_EXCHANGE),
-                new Queue(QUEUE_TASK),
-                new Queue(QUEUE_TASK_RETRY),
-                new Binding(QUEUE_TASK, Binding.DestinationType.QUEUE,
-                        TASK_DIRECT_EXCHANGE, ROUTING_KEY_TASK, null),
-                new Binding(QUEUE_TASK_RETRY, Binding.DestinationType.QUEUE,
-                        TASK_DIRECT_EXCHANGE, ROUTING_KEY_TASK_RETRY, null));
+    Queue createQueue() {
+        return new Queue(QUEUE_TASK, false);
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory factory) {
-        RabbitTemplate template = new RabbitTemplate(factory);
-        template.setMessageConverter(new Jackson2JsonMessageConverter());
-        System.out.println("[DEBUG] HELLO RABBIT TEMPLATE ?");
-        return template;
+    DirectExchange exchange() {
+        return new DirectExchange(TASK_DIRECT_EXCHANGE);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY_TASK);
+    }
+//    @Bean
+//    public Declarables createTaskScheduleSchema() {
+//        System.out.println("[DEBUG] HELLO DECLARE X & Q");
+//        return new Declarables(
+//                new DirectExchange(TASK_DIRECT_EXCHANGE),
+//                new Queue(QUEUE_TASK),
+//                new Queue(QUEUE_TASK_RETRY),
+//                new Binding(QUEUE_TASK, Binding.DestinationType.QUEUE,
+//                        TASK_DIRECT_EXCHANGE, ROUTING_KEY_TASK, null),
+//                new Binding(QUEUE_TASK_RETRY, Binding.DestinationType.QUEUE,
+//                        TASK_DIRECT_EXCHANGE, ROUTING_KEY_TASK_RETRY, null));
+//    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
     }
 }
