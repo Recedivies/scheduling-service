f94779d96fc348004b18cc6252de74d95a376600
recedivies <ahmadhiprananta@gmail.com>
2024-04-05 16:27:11 +0700

    Update CI

--- src/main/java/com/example/scheduling_service/config/RabbitMQConfig.java
+++ src/main/java/com/example/scheduling_service/config/RabbitMQConfig.java
@@ -1,9 +1,7 @@
 package com.example.scheduling_service.config;
 
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
